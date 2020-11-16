6807681dddd05eb2175c20cee3f2d601c6cef7c1
CDub19 <chriswilliamson@live.com>
2020-11-16 12:41:44 -0800

    Code

--- flyPlateBuffs.lua
+++ flyPlateBuffs.lua
@@ -619,12 +619,11 @@ local function CreateBuffIcon(frame,i)
 end
 
 local function UpdateUnitAuras(nameplateID,updateOptions)
-	local number = string.match(nameplateID, "%d+")
-	local TPAnchor = _G["ThreatPlatesFrameNamePlate"..number]
+	--local number = string.match(nameplateID, "%d+")
+	--local TPAnchor = _G["ThreatPlatesFrameNamePlate"..number]
 
-	local frame = TPAnchor or C_NamePlate_GetNamePlateForUnit(nameplateID)
+	local frame = 	_G["ThreatPlatesFrame"..(C_NamePlate_GetNamePlateForUnit(nameplateID):GetName())] or C_NamePlate_GetNamePlateForUnit(nameplateID)
 	if not frame then return end 	-- modifying friendly nameplates is restricted in instances since 7.2
-
 	if FilterUnits(nameplateID) then
 		if frame.fPBiconsFrame then
 			frame.fPBiconsFrame:Hide()
